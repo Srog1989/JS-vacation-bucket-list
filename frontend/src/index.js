11193b9a45282a36f9c2442598a2e50bf95b87be
Srog1989 <s.rogers0715@yahoo.com>
2021-02-17 18:56:00 -0700

    configured index.js file

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+const apiService = new ApiService()
+// const apiService = `http://localhost:3000`
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("hello")
+    Destination.generateDestinations()
+
+const submit = document.querySelector("#submitButton")
+
+//  submitButton.addEventListener("click", function(e) => {
+//     e.preventdefault
+// })
+})
