395c33d4bc3fb18cc8eceb85bf5a958b103b37ab
Srog1989 <s.rogers0715@yahoo.com>
2021-02-17 19:47:31 -0700

    seeded database

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    # has_many :destinations
+    has_many :destinations
 
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sarah = User.create(name: "Sarah")
+jordan = User.create(name: "Jordan")
+mackenzie = User.create(name: "Mackenzie")
+
+destination = sarah.destinations.build(city: "Dublin", country: "Ireland", importance: 3 )
+destination.save
+vacation = jordan.destinations.build(city: "Rome", country: "Italy", importance: 4 )
+vacation.save
+location = mackenzie.destinations.build(city: "Paris", country: "France", importance: 5)
+location.save
+
