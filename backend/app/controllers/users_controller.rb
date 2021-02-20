87c07560677571c1066d22b3f40af28b9626d0b7
Srog1989 <s.rogers0715@yahoo.com>
2021-02-19 20:27:07 -0700

    modified users_controller create action

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
     def index
-        
+        users = User.all
+        render json: users
     end
 
     def create
-
+        
+        user = User.find_by(name: params[:name])
+        if user
+            render json: user, include: [:destinations]
+            # binding.pry
+        else
+            newUser = User.create(name: params[:name])
+            render json: newUser, include: [:destinations]
+            
+        end
     end
 
     def destroy
-        user = User.find(params[:id])
-        user.destroy
+        # user = User.find(params[:id])
+        # user.destroy
     end
 end
