4ab3a70342eb72950a74fa74fb3e7f91070aa606
Srog1989 <s.rogers0715@yahoo.com>
2021-02-20 20:48:08 -0700

    modified render destinations and wrote renderUserDestinations

--- frontend/src/destination.js
+++ frontend/src/destination.js
@@ -14,11 +14,35 @@ class Destination {
             .then(data => 
                 data.forEach(destination => {
                     const newDestination = new Destination(destination)
-                    // console.log(newDestination)
+                    Destination.renderDestinations(destination)
                 })
             )
     }
 
+    static renderDestinations(destination) {
+        const ul = document.querySelector("#List-of-Destinations")
+            const li = document.createElement("li")
+            li.innerHTML = destination.city
+            ul.appendChild(li)
+         }
 
+         renderUserGratitudes(){
+            const list = document.querySelector(".destinations-container")
+                this.destinations.forEach(destination =>{
+                    const div = document.createElement("div")
+                    const h2 = document.createElement("h2")
+                    const h3 = document.createElement("h3")
+                    const h4 = document.createElement("h4")
+                    const p = document.createElement("p")
+                    div.setAttribute("class", "card")
+                    div.setAttribute("data-id", destination.id)
+                    h3.innerHTML = destination.city
+                    h4.innerHTML = `Country: ${destination.country}`
+                    p.innerHTML = `Importance: ${destination.importance}`
+                    div.appendChild(h3)
+                    div.appendChild(h4)
+                    div.appendChild(p)
+                    list.appendChild(div)
+                })
 
 }
