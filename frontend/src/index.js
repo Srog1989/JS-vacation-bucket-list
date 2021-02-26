b2a12790aab1fa893aeb27578bd55d875cdec82e
Srog1989 <s.rogers0715@yahoo.com>
2021-02-26 14:56:21 -0700

    refactory index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,6 @@
 const apiService = new ApiService()
-// const apiService = `http://localhost:3000`
-
 
 document.addEventListener('DOMContentLoaded', function(){
-    console.log("hello")
     Destination.generateDestinations()
 
     const userLogin = document.getElementById("new-user-form")
@@ -11,11 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
     userLogin.addEventListener("submit", function(e){
         e.preventDefault()
         const userName = e.target.username.value
-        User.generateUser(userName)
-
-        
-        
+        User.generateUser(userName)  
     })
-
 
 })  
