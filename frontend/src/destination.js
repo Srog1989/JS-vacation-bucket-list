72e9c1bda134d1bfa2747fed5176da9a4d386163
Srog1989 <s.rogers0715@yahoo.com>
2021-02-26 14:57:36 -0700

    wrote traveled static method and called in renderDestinations

--- frontend/src/destination.js
+++ frontend/src/destination.js
@@ -15,26 +15,26 @@ class Destination {
             .then(data => 
                 data.forEach(destination => {
                     const newDestination = new Destination(destination)
-                    Destination.renderDestinations(destination)
+                    Destination.renderDestinationList(newDestination)
                 })
             )
     }
 
-    static renderDestinations(destination) {
+    static renderDestinationList(destination) {
         const ul = document.querySelector("#List-of-Destinations")
-            const li = document.createElement("li")
-            li.innerHTML = destination.city
-            ul.appendChild(li)
+        const li = document.createElement("li")
+        li.innerHTML = destination.city
+        ul.appendChild(li)
          }
 
     static destinationEvents(userId){
-        let destinationEvent = document.querySelector(".new-destination-form")
-        destinationEvent.addEventListener("submit", function(e){
-            e.preventDefault()
+            let destinationEvent = document.querySelector(".new-destination-form")
+                destinationEvent.addEventListener("submit", function(e){
+                e.preventDefault()
             const dCity = e.target.city.value
             const dCountry = e.target.country.value
             const dImportance = e.target.importance.value
-            Destination.createDestination(dCity, dCountry, dImportance, userId)
+                Destination.createDestination(dCity, dCountry, dImportance, userId)
         })
     }
 
@@ -43,10 +43,45 @@ class Destination {
             .then(destinationData => 
                 {
                     const newDestination = new Destination(destinationData)
-                    // Destination.renderDestinations(newDestination)
-                    console.log(newDestination)
+                    Destination.renderDestination(newDestination)
                 })
             
+    }
+
+    
+    static renderDestination(destination) {
+        const destinationContainer = document.querySelector(".destinations-container")
+        const div = document.createElement("div")
+        const h2 = document.createElement("h2")
+        const h3 = document.createElement("h3")
+        const h4 = document.createElement("h4")
+        const button = document.createElement("button")
+
+        div.setAttribute("class", "card")
+        div.setAttribute("data-id", destination.id)
+        button.setAttribute("class", "delete-destination")
+        button.setAttribute("data-destination-id", destination.id)
+
+        h2.innerHTML = destination.city
+        h3.innerHTML = `Country: ${destination.country}`
+        h4.innerHTML = `Importance: ${destination.importance}`
+        button.innerHTML = "Traveled"
+
+
+        div.appendChild(h2)
+        div.appendChild(h3)
+        div.appendChild(h4)
+        div.appendChild(button)
+        destinationContainer.appendChild(div)
+        this.traveled(button)
+    }
+
+    static traveled(button){
+        button.addEventListener('click', function(e){
+            e.preventDefault()
+            apiService.fetchDeleteDestination(e.target.dataset.destinationId)
+            e.target.parentElement.remove();
+        })
     }
 
 
